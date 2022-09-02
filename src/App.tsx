97432d041b3689df93d457f6019851d9ab9422fc
divinityMaker <truedmaker@gmail.com>
2022-09-02 17:07:15 -0300

    feat: added pokemon API

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,22 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react'
+import PokemonAPI from './services/getPokemons'
+import { Pokemon } from './types/PokemonType'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+  const [pokemon, setPokemon] = useState<Pokemon[]>()
+  useEffect(() => {
+    const apiCall = async (): Promise<void> => {
+      const response = await PokemonAPI.getPokemonData()
+      setPokemon(response)
+    }
+    void apiCall()
+  }, [])
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+  if (pokemon === undefined) return <h1>Loading</h1>
+
+  return (<h1>{pokemon.map(item => {
+    return <p key={item.id}>{item.name}</p>
+  })}</h1>)
 }
 
 export default App
