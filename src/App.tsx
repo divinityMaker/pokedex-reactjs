7bf6041c7e6690943c3c04203f8c37650e28c548
divinityMaker <truedmaker@gmail.com>
2022-09-03 21:36:11 -0300

    feat: added infinite scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,38 @@ import { Pokemon } from './types/PokemonType'
 
 const App: React.FC = () => {
   const [pokemon, setPokemon] = useState<Pokemon[]>()
+  let offset = 30
   useEffect(() => {
+    const handleScroll = (e): void => {
+      if (
+        window.innerHeight + parseInt(e.target.documentElement.scrollTop) + 1 >=
+        parseInt(e.target.documentElement.scrollHeight)
+      ) {
+        offset = offset += 10
+        const infiniteScrolling = async (): Promise<void> => {
+          const response = await PokemonAPI.loadMorePokemons(offset)
+          setPokemon(state => [...state as Pokemon[], ...response])
+        }
+        void infiniteScrolling()
+      }
+    }
     const apiCall = async (): Promise<void> => {
       const response = await PokemonAPI.getPokemonData()
       setPokemon(response)
     }
     void apiCall()
+    window.addEventListener('scroll', handleScroll)
   }, [])
 
   if (pokemon === undefined) return <h1>Loading</h1>
 
   return (<h1>{pokemon.map(item => {
-    return <p key={item.id}>{item.name}</p>
+    return (
+      <>
+        <img key={item.id} src={item.sprites.front_default} />
+        <br />
+      </>
+    )
   })}</h1>)
 }
 
--- src/services/getPokemons.ts
+++ src/services/getPokemons.ts
@@ -3,7 +3,18 @@ import { Pokemon } from '../types/PokemonType'
 
 const PokemonAPI = {
   getPokemonData: async () => {
-    const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=100')
+    const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=30&offset=0')
+    const pokemons = response.data.results
+    const newPokemon: Pokemon[] = []
+
+    for (const pokemon of pokemons) {
+      const response = await axios.get(pokemon.url)
+      newPokemon.push(response.data)
+    }
+    return newPokemon
+  },
+  loadMorePokemons: async (offset: number) => {
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${offset}`)
     const pokemons = response.data.results
     const newPokemon: Pokemon[] = []
 
